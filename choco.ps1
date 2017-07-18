fd09a841b415fee5421d34a3fb493a0060b94621
Antonio Bengelsdorff <antonio.bengelsdorff@gmail.com>
2017-07-18 16:55:40 +0100

    Initial Release

---
+++ choco.ps1
@@ -0,0 +1,24 @@
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install git.install
+choco install sourcetree
+choco install tortoisehg
+choco install nodejs.install
+choco install openvpn
+choco install dotnetcore
+choco install visualstudiocode
+choco install visualstudio2017community
+choco install visualstudio2017-workload-netweb
+choco install visualstudio2017-workload-netcoretools
+choco install visualstudio2017-workload-azure
+choco install notepadplusplus.install
+choco install googlechrome
+choco install sql-server-management-studio
+choco install skype
+choco install 7zip.install
+choco install procexp
+choco install curl
+choco install fiddler4
+choco install evernote
+choco install spotify
+choco install mremoteng
