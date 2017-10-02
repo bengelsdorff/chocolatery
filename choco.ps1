b15304caa53e2d830e40fdb4f82599fd9035d265
Antonio Bengelsdorff <antonio.bengelsdorff@gmail.com>
2017-10-02 12:54:44 +0200

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,5 +1,7 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco feature enable -n allowGlobalConfirmation
+
 choco install git.install
 choco install sourcetree
 choco install gitkraken
